8419c920673c6c4385b0c5f29453cf7b71c2517e
sujan0991 <sujan0991@gmail.com>
2024-07-14 20:13:58 +0200

    full cimmit

--- script.py
+++ script.py
@@ -1,166 +1,92 @@
-   
-# Importing Libraries
-import numpy as np
-import pandas as pd
-from sklearn.neighbors import NearestNeighbors
-import matplotlib.pyplot as plt
+# performing linear algebra 
+import numpy as np 
+# data processing 
+import pandas as pd 
+# visualisation 
+import matplotlib.pyplot as plt 
 import seaborn as sns
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
-
-#loading rating dataset
-ratings = pd.read_csv("https://s3-us-west-2.amazonaws.com/recommender-tutorial/ratings.csv")
-#print(ratings.head())
-
-# loading movie dataset
-movies = pd.read_csv("https://s3-us-west-2.amazonaws.com/recommender-tutorial/movies.csv")
-#print(movies.head())
-
-
-# Statistical Analysis of Ratings
-
-n_ratings = len(ratings)
-n_movies = len(ratings['movieId'].unique())
-n_users = len(ratings['userId'].unique())
-
-print(f"Number of ratings: {n_ratings}")
-print(f"Number of unique movieId's: {n_movies}")
-print(f"Number of unique users: {n_users}")
-print(f"Average ratings per user: {round(n_ratings/n_users, 2)}")
-print(f"Average ratings per movie: {round(n_ratings/n_movies, 2)}")
 
 
 
-# User Rating Frequency
+df = pd.read_csv("/Users/apple/Desktop/Data_science_projects/Cancer_ prediction_KNN/data.csv") 
 
-user_freq = ratings[['userId', 'movieId']].groupby(
-    'userId').count().reset_index()
-user_freq.columns = ['userId', 'n_ratings']
-print(user_freq.head())
+print(df.head) 
 
-# Movie Rating Analysis
+#df.info()
 
-# Find Lowest and Highest rated movies:
-mean_rating = ratings.groupby('movieId')[['rating']].mean()
-# Lowest rated movies
-lowest_rated = mean_rating['rating'].idxmin()
-movies.loc[movies['movieId'] == lowest_rated]
-# Highest rated movies
-highest_rated = mean_rating['rating'].idxmax()
-movies.loc[movies['movieId'] == highest_rated]
-# show number of people who rated movies rated movie highest
-ratings[ratings['movieId']==highest_rated]
-# show number of people who rated movies rated movie lowest
-ratings[ratings['movieId']==lowest_rated]
+df = df.drop(['Unnamed: 32', 'id'], axis = 1) 
+print(df.shape) 
 
-## the above movies has very low dataset. We will use bayesian average
-movie_stats = ratings.groupby('movieId')[['rating']].agg(['count', 'mean'])
-movie_stats.columns = movie_stats.columns.droplevel()
+# Converting the diagnosis value of M and B to a numerical value where M (Malignant) = 1 and B (Benign) = 0
 
 
-# User-Item Matrix Creation
+def diagnosis_value(diagnosis): 
+	if diagnosis == 'M': 
+		return 1
+	else: 
+		return 0
 
-   
-# Now, we create user-item matrix using scipy csr matrix
-from scipy.sparse import csr_matrix
-
-
-
-def create_matrix(df):
-	
-	N = len(df['userId'].unique())
-	M = len(df['movieId'].unique())
-	
-	# Map Ids to indices
-	user_mapper = dict(zip(np.unique(df["userId"]), list(range(N))))
-	movie_mapper = dict(zip(np.unique(df["movieId"]), list(range(M))))
-	
-	# Map indices to IDs
-	user_inv_mapper = dict(zip(list(range(N)), np.unique(df["userId"])))
-	movie_inv_mapper = dict(zip(list(range(M)), np.unique(df["movieId"])))
-	
-	user_index = [user_mapper[i] for i in df['userId']]
-	movie_index = [movie_mapper[i] for i in df['movieId']]
-
-	X = csr_matrix((df["rating"], (movie_index, user_index)), shape=(M, N))
-	
-	return X, user_mapper, movie_mapper, user_inv_mapper, movie_inv_mapper
-	
+df['diagnosis'] = df['diagnosis'].apply(diagnosis_value) 
 
 
-X, user_mapper, movie_mapper, user_inv_mapper, movie_inv_mapper = create_matrix(ratings)
+# sns.lmplot(x = 'radius_mean', y = 'texture_mean', hue = 'diagnosis', data = df) 
+
+# sns.lmplot(x ='smoothness_mean', y = 'compactness_mean', data = df, hue = 'diagnosis') 
+
+# Input and Output data
+
+X = np.array(df.iloc[:, 1:]) 
+y = np.array(df['diagnosis']) 
 
 
-# Movie Similarity Analysis
+# Splitting data to training and testing
 
-"""
-Find similar movies using KNN
-"""
-def find_similar_movies(movie_id, X, k, metric='cosine', show_distance=False):
-	
-	neighbour_ids = []
-	
-	movie_ind = movie_mapper[movie_id]
-	movie_vec = X[movie_ind]
-	k+=1
-	kNN = NearestNeighbors(n_neighbors=k, algorithm="brute", metric=metric)
-	kNN.fit(X)
-	movie_vec = movie_vec.reshape(1,-1)
-	neighbour = kNN.kneighbors(movie_vec, return_distance=show_distance)
-	for i in range(0,k):
-		n = neighbour.item(i)
-		neighbour_ids.append(movie_inv_mapper[n])
-	neighbour_ids.pop(0)
-	return neighbour_ids
+from sklearn.model_selection import train_test_split 
+
+X_train, X_test, y_train, y_test = train_test_split( 
+	X, y, test_size = 0.33, random_state = 42) 
 
 
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors = 13) 
+knn.fit(X_train, y_train) 
+
+# Prediction Score
+
+print('Prediction score',knn.score(X_test, y_test) )
+
+# Performing Cross Validation
+
+neighbors = [] 
+cv_scores = [] 
+
+from sklearn.model_selection import cross_val_score 
+
+# perform 10 fold cross validation 
+for k in range(1, 51, 2): 
+	neighbors.append(k) 
+	knn = KNeighborsClassifier(n_neighbors = k) 
+	scores = cross_val_score( 
+		knn, X_train, y_train, cv = 10, scoring = 'accuracy') 
+	cv_scores.append(scores.mean()) 
 
 
-movie_titles = dict(zip(movies['movieId'], movies['title']))
+# Misclassification error versus k
 
-movie_id = 2
+MSE = [1-x for x in cv_scores] 
 
-similar_ids = find_similar_movies(movie_id, X, k=10)
-movie_title = movie_titles[movie_id]
+# determining the best k 
+optimal_k = neighbors[MSE.index(min(MSE))] 
+print('The optimal number of neighbors is % d ' % optimal_k) 
 
-print(f"Since you watched {movie_title}")
-for i in similar_ids:
-	print(movie_titles[i])
-
-
-
-
-# Movie Recommendation with respect to Users Preference
-
-
-def recommend_movies_for_user(user_id, X, user_mapper, movie_mapper, movie_inv_mapper, k=10):
-	df1 = ratings[ratings['userId'] == user_id]
-	
-	if df1.empty:
-		print(f"User with ID {user_id} does not exist.")
-		return
-
-	movie_id = df1[df1['rating'] == max(df1['rating'])]['movieId'].iloc[0]
-
-	movie_titles = dict(zip(movies['movieId'], movies['title']))
-
-	similar_ids = find_similar_movies(movie_id, X, k)
-	movie_title = movie_titles.get(movie_id, "Movie not found")
-
-	if movie_title == "Movie not found":
-		print(f"Movie with ID {movie_id} not found.")
-		return
-
-	print(f"Since you watched {movie_title}, you might also like:")
-	for i in similar_ids:
-		print(movie_titles.get(i, "Movie not found"))
+# plot misclassification error versus k 
+plt.figure(figsize = (10, 6)) 
+plt.plot(neighbors, MSE) 
+plt.xlabel('Number of neighbors') 
+plt.ylabel('Misclassification Error') 
+plt.show() 
 
 
-
-# Reccomment the movies
-
-user_id = 150 # Replace with the desired user ID
-recommend_movies_for_user(user_id, X, user_mapper, movie_mapper, movie_inv_mapper, k=10)
 
 
