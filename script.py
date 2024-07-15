2384e864b51b56a9f68082758f020e6650b7ab2e
sujan0991 <sujan0991@gmail.com>
2024-07-15 21:02:38 +0200

    full commit

--- script.py
+++ script.py
@@ -1,92 +1,128 @@
-# performing linear algebra 
-import numpy as np 
-# data processing 
+
 import pandas as pd 
-# visualisation 
+import numpy as np 
 import matplotlib.pyplot as plt 
-import seaborn as sns
+import seaborn as sns 
+
+data = pd.read_csv("/Users/apple/Desktop/Data_science_projects/Loan_Approval_Prediction/LoanApprovalPrediction.csv") 
+
+data.head(5)
 
 
+# Data Preprocessing and Visualization
 
-df = pd.read_csv("/Users/apple/Desktop/Data_science_projects/Cancer_ prediction_KNN/data.csv") 
+# Get the number of columns of object datatype
+obj = (data.dtypes == 'object') 
+print("Categorical variables:",len(list(obj[obj].index)))
 
-print(df.head) 
+# Dropping Loan_ID column 
+data.drop(['Loan_ID'],axis=1,inplace=True)
 
-#df.info()
+# Visualize all the unique values in columns
 
-df = df.drop(['Unnamed: 32', 'id'], axis = 1) 
-print(df.shape) 
+obj = (data.dtypes == 'object') 
+object_cols = list(obj[obj].index) 
+plt.figure(figsize=(18,36)) 
+index = 1
 
-# Converting the diagnosis value of M and B to a numerical value where M (Malignant) = 1 and B (Benign) = 0
-
-
-def diagnosis_value(diagnosis): 
-	if diagnosis == 'M': 
-		return 1
-	else: 
-		return 0
-
-df['diagnosis'] = df['diagnosis'].apply(diagnosis_value) 
-
-
-# sns.lmplot(x = 'radius_mean', y = 'texture_mean', hue = 'diagnosis', data = df) 
-
-# sns.lmplot(x ='smoothness_mean', y = 'compactness_mean', data = df, hue = 'diagnosis') 
-
-# Input and Output data
-
-X = np.array(df.iloc[:, 1:]) 
-y = np.array(df['diagnosis']) 
+for col in object_cols: 
+	y = data[col].value_counts() 
+	plt.subplot(11,4,index) 
+	plt.xticks(rotation=90) 
+	sns.barplot(x=list(y.index), y=y)
+	#plt.show()
+	index +=1
 
 
-# Splitting data to training and testing
+# categorical values into int datatype
+
+# Import label encoder 
+from sklearn import preprocessing 
+	
+# label_encoder object knows how 
+# to understand word labels. 
+label_encoder = preprocessing.LabelEncoder() 
+obj = (data.dtypes == 'object') 
+for col in list(obj[obj].index): 
+    data[col] = label_encoder.fit_transform(data[col])
+
+
+# To find the number of columns with 
+# datatype==object 
+obj = (data.dtypes == 'object') 
+print("Categorical variables:",len(list(obj[obj].index)))
+
+
+plt.figure(figsize=(12,6)) 
+
+sns.heatmap(data.corr(),cmap='BrBG',fmt='.2f', 
+			linewidths=2,annot=True)
+
+plt.show()
+
+
+sns.catplot(x="Gender", y="Married", 
+			hue="Loan_Status", 
+			kind="bar", 
+			data=data)
+
+plt.show()
+
+
+# check missing values
+
+for col in data.columns: 
+    data[col] = data[col].fillna(data[col].mean()) 
+	
+data.isna().sum()
+
+# Splitting Dataset
 
 from sklearn.model_selection import train_test_split 
 
-X_train, X_test, y_train, y_test = train_test_split( 
-	X, y, test_size = 0.33, random_state = 42) 
+X = data.drop(['Loan_Status'],axis=1) 
+Y = data['Loan_Status'] 
+X.shape,Y.shape 
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, 
+													test_size=0.4, 
+													random_state=1) 
+X_train.shape, X_test.shape, Y_train.shape, Y_test.shape
 
 
-from sklearn.neighbors import KNeighborsClassifier
+# Model Training and Evaluation
 
-knn = KNeighborsClassifier(n_neighbors = 13) 
-knn.fit(X_train, y_train) 
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.svm import SVC 
+from sklearn.linear_model import LogisticRegression 
 
-# Prediction Score
+from sklearn import metrics 
 
-print('Prediction score',knn.score(X_test, y_test) )
+knn = KNeighborsClassifier(n_neighbors=3) 
+rfc = RandomForestClassifier(n_estimators = 7, 
+							criterion = 'entropy', 
+							random_state =7) 
+svc = SVC() 
+lc = LogisticRegression() 
 
-# Performing Cross Validation
-
-neighbors = [] 
-cv_scores = [] 
-
-from sklearn.model_selection import cross_val_score 
-
-# perform 10 fold cross validation 
-for k in range(1, 51, 2): 
-	neighbors.append(k) 
-	knn = KNeighborsClassifier(n_neighbors = k) 
-	scores = cross_val_score( 
-		knn, X_train, y_train, cv = 10, scoring = 'accuracy') 
-	cv_scores.append(scores.mean()) 
-
-
-# Misclassification error versus k
-
-MSE = [1-x for x in cv_scores] 
-
-# determining the best k 
-optimal_k = neighbors[MSE.index(min(MSE))] 
-print('The optimal number of neighbors is % d ' % optimal_k) 
-
-# plot misclassification error versus k 
-plt.figure(figsize = (10, 6)) 
-plt.plot(neighbors, MSE) 
-plt.xlabel('Number of neighbors') 
-plt.ylabel('Misclassification Error') 
-plt.show() 
+# making predictions on the training set 
+for clf in (rfc, knn, svc,lc): 
+	clf.fit(X_train, Y_train) 
+	Y_pred = clf.predict(X_train) 
+	print("Accuracy score of ", 
+		clf.__class__.__name__, 
+		"=",100*metrics.accuracy_score(Y_train, Y_pred))
 
 
 
+# making predictions on the testing set 
+for clf in (rfc, knn, svc,lc): 
+	clf.fit(X_train, Y_train) 
+	Y_pred = clf.predict(X_test) 
+	print("Accuracy score of ", 
+		clf.__class__.__name__,"=", 
+		100*metrics.accuracy_score(Y_test, Y_pred))
 
+
+# to get much better results ensemble learning techniques like Bagging and Boosting can also be used.
